7a72386919576854740579dd6ff211befa71aa7d
rroma995 <65393767+rroma995@users.noreply.github.com>
2020-08-14 00:08:53 +0900

    Update other.py

--- cogs/other.py
+++ cogs/other.py
@@ -120,10 +120,10 @@ class 기타(commands.Cog):
         """포코봇 사이트 주소를 보여줍니다"""
         await ctx.send("https://poco2020bot.netlify.app/")
         
-    @commands.command(name="포코봇 공식디코", pass_context=True)
+    @commands.command(name="포코봇공식디스코드", pass_context=True)
     async def fidbak(self, ctx):
         """포코봇 공식 디스코드 주소를 보여줍니다"""
-        await ctx.send("포코봇 공식디스코드 방이 준비중 입니다.")
+        await ctx.send("https://discord.gg/S6b3XnT")
 
     @commands.command(name="온라인")
     async def servernumber(self, ctx):
